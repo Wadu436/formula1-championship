e5c67283db6a42438077d0d02171ccdea238d521
Warre Dujardin <warre.dujardin@gmail.com>
2021-12-12 00:03:27 +0100

    add leaderboard index page

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # ex: /api/driver/5/team
     path("api/driver/", views.get_drivers, name="get_drivers"),
     path("api/driver/<int:driver_id>", views.get_driver, name="get_driver"),
+    path("", views.leaderboard_page, name="leaderboards"),
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -3,7 +3,7 @@ import json
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Driver
+from .models import Championship, Driver
 
 
 # Create your views here.
@@ -17,3 +17,8 @@ def get_drivers(request):
 def get_driver(request, driver_id):
     driver = Driver.objects.get(pk=driver_id)
     return HttpResponse(json.dumps(driver.get_dict()), content_type="application/json")
+
+
+def leaderboard_page(request):
+    context = {"championships": Championship.objects.all()}
+    return render(request, "leaderboard/leaderboard_page.html", context=context)

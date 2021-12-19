100727faf31c6481c4153a079b56194712133e27
Warre Dujardin <warre.dujardin@gmail.com>
2021-12-20 00:50:02 +0100

    update races

--- app/leaderboard/views.py
+++ app/leaderboard/views.py
@@ -22,29 +22,55 @@ def get_driver(request, driver_id):
 
 # HTML views
 def drivers_standings(request, championship_id):
-    context = {
-        "current_championship": Championship.objects.get(id=championship_id),
-        "championships": Championship.objects.all(),
-    }
-    return render(request, "leaderboard/drivers_standings.html", context=context)
+    championship = Championship.objects.filter(id=championship_id).first()
+    if championship:
+        context = {
+            "current_championship": championship,
+            "championships": Championship.objects.all(),
+        }
+        return render(request, "leaderboard/drivers_standings.html", context=context)
+    else:
+        return latest_drivers_standings(request)
 
 
 def constructors_standings(request, championship_id):
-    context = {
-        "current_championship": Championship.objects.get(id=championship_id),
-        "championships": Championship.objects.all(),
-    }
-    return render(request, "leaderboard/constructors_standings.html", context=context)
+    championship = Championship.objects.filter(id=championship_id).first()
+    if championship:
+        context = {
+            "current_championship": Championship.objects.get(id=championship_id),
+            "championships": Championship.objects.all(),
+        }
+        return render(
+            request, "leaderboard/constructors_standings.html", context=context
+        )
+    else:
+        return latest_constructors_standings(request)
 
 
+def races(request, championship_id):
+    championship = Championship.objects.filter(id=championship_id).first()
+    if championship:
+
+        context = {
+            "current_championship": Championship.objects.get(id=championship_id),
+            "championships": Championship.objects.all(),
+        }
+        return render(request, "leaderboard/races.html", context=context)
+    else:
+        return latest_races(request)
+
+
+# Latest redirect views
 def latest_drivers_standings(request):
     latest_championship = Championship.objects.latest()
     return redirect(reverse("drivers_standings", args=[latest_championship.id]))
 
 
-def races(request, championship_id):
-    context = {
-        "current_championship": Championship.objects.get(id=championship_id),
-        "championships": Championship.objects.all(),
-    }
-    return render(request, "leaderboard/races.html", context=context)
+def latest_constructors_standings(request):
+    latest_championship = Championship.objects.latest()
+    return redirect(reverse("constructors_standings", args=[latest_championship.id]))
+
+
+def latest_races(request):
+    latest_championship = Championship.objects.latest()
+    return redirect(reverse("races", args=[latest_championship.id]))

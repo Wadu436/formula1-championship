5f457a7da8c41e918e58d9b4add2e17ca4fc6c1f
Warre Dujardin <warre.dujardin@gmail.com>
2022-02-05 17:58:24 +0100

    set up debugging

--- app/manage.py
+++ app/manage.py
@@ -3,10 +3,19 @@
 import os
 import sys
 
+from django.conf import settings
+
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import debugpy
+            debugpy.listen(("0.0.0.0", 3000))
+            print('Attached!')
+
+    """Run administrative tasks."""
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
